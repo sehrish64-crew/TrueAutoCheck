32e69912463d85a0bf1abe05f490e7be9f691bca
Sehrish <sehrish64.crew@gmail.com>
2026-03-02 04:34:51 -0800

    Remove country location popup component and references

--- components/LayoutWrapper.tsx
+++ components/LayoutWrapper.tsx
@@ -3,8 +3,7 @@
 import { usePathname } from 'next/navigation'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
-import { CountryProvider } from '@/contexts/CountryContext'
-import LocationPopup from '@/components/LocationPopup'
+import { CountryProvider } from '@/contexts/CountryContext' 
 
 export default function LayoutWrapper({ children }: { children: React.ReactNode }) {
   const pathname = usePathname()
@@ -21,7 +20,6 @@ export default function LayoutWrapper({ children }: { children: React.ReactNode 
 
   return (
     <CountryProvider>
-      <LocationPopup />
       <Header />
       {children}
       <Footer />
